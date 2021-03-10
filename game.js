81e14ebfc114ad7f8ee2b02ef7d058bfe751858e
m0n411111 <bjn.ant1@gmail.com>
2021-03-10 12:59:24 +0100

    .

--- game.js
+++ game.js
@@ -1,21 +1,45 @@
 const container = document.querySelector('#container')
 const clearButton = document.querySelector('#clear')
 
+window.addEventListener("load", setDefaultGrid);
+clearButton.addEventListener("click", changeSize);
+
 function setDefaultGrid() {
     setGridSize(16);
-    fillGrid(16);
+    createGrid(16);
   }
   
   function setGridSize(size) {
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   }
   
-  function fillGrid(size) {
-    for (let i = 0; i < size * size; i++) {
-      const gridElement = document.createElement("div");
-      gridElement.classList = "grid-element";
-      gridElement.addEventListener("mouseover", changeColor);
-      container.appendChild(gridElement);
+  function createGrid(size) {
+    for (let i = 0; i < 256; i++) {
+      const div = document.createElement("div");
+      div.classList.add('grid-element');
+      container.appendChild(div);
     }
   }
   
+  function changeSize() {
+    let newSize = prompt("Enter new size");
+  
+    if (newSize !== null) {
+      newSize = parseInt(newSize);
+      if (newSize < 1 || newSize > 64 || Number.isNaN(newSize)) {
+        alert("Enter a number from 1-64 range");
+        changeSize();
+      } else {
+        clearGrid();
+        setGridSize(newSize);
+        createGrid(newSize);
+      }
+    }
+  }
+  
+  function clearGrid() {
+    const gridArray = Array.from(container.childNodes);
+    gridArray.forEach((element) => {
+      container.removeChild(element);
+    });
+  }
