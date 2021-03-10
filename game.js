9d0dab938b9d1238fc7499fe544db74b0c34a5b5
m0n411111 <bjn.ant1@gmail.com>
2021-03-10 12:11:40 +0100

    .

--- game.js
+++ game.js
@@ -1,17 +1,11 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('#container')
 const clearButton = document.querySelector('#clear')
 
-let squareNum = 16
-
-function createGrid(squareNum) { 
+let squareNum = 16;
 
 function createDiv(num) {
-    for (let i = 0; i < num * num; i++) {
-
     const div = document.createElement('div');
     div.classList.add('grid');
+    div.setAttribute('style', `width: ${num}px; height: ${num}px`);
     container.appendChild(div);
-    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-}
-}
 }
